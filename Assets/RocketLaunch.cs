ef7e2f296f5c33480907b04cffd3fa695179f3ed
PioneeShark <wuyi@mail.com>
2024-01-27 19:42:15 +0000

    Rocket Launch base

--- Assets/RocketLaunch.cs
+++ Assets/RocketLaunch.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class RocketLaunch : BaseRangedWeapon
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
